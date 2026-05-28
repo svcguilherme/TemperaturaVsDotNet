a0749625c758c7c26f43d71aee0eaf0e719465d4
Guilherme Sant Anna <=>
2026-05-28 14:35:01 -0300

    Initial

--- api-person/obj/Debug/net8.0/PersonApi.AssemblyInfo.cs
+++ api-person/obj/Debug/net8.0/PersonApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PersonApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bfe52a3228b4fbcd2945ff3d44a0ef2c82f4f8cb")]
 [assembly: System.Reflection.AssemblyProductAttribute("PersonApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PersonApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
